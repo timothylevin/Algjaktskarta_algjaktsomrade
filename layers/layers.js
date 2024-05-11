a2a5cfce1a7a9265506d9281af733d4209098e19
timothylevin <167991082+timothylevin@users.noreply.github.com>
2024-05-12 00:55:56 +0200

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,32 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var lyr_lgjaktomrden_1 = new ol.layer.Tile({
+                            source: new ol.source.TileWMS(({
+                              url: "https://ext-geodata-applikationer.lansstyrelsen.se/arcgis/services/Jaktadm/lst_jaktadm_visning/MapServer/WMSServer",
+    attributions: ' ',
+                              params: {
+                                "LAYERS": "1",
+                                "TILED": "true",
+                                "VERSION": "1.3.0"},
+                            })),
+                            title: "Älgjaktområden",
+                            opacity: 1.000000,
+                            
+                            
+                          });
+              wms_layers.push([lyr_lgjaktomrden_1, 1]);
+
+lyr_OSMStandard_0.setVisible(true);lyr_lgjaktomrden_1.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_lgjaktomrden_1];
